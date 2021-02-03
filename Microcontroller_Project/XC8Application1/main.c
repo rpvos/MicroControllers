a9c6d6cd31a275edd77671ddb2b6583dd28b8de3
Dave-Avans <dl.visser@student.avans.nl>
2021-02-03 10:18:14 +0100

    [Add]: Opdracht B_2

---
+++ Microcontroller_Project/XC8Application1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/3/2021 10:13:30 AM
+ *  Author: Dave-
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
