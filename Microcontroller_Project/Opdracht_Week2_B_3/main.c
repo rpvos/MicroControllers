ccb421c09ab24bd6c86ecb5e1ad72627313805d3
Dave-Avans <dl.visser@student.avans.nl>
2021-02-10 11:26:31 +0100

    [Add]: next save.

--- Microcontroller_Project/Opdracht_Week2_B_3/main.c
+++ Microcontroller_Project/Opdracht_Week2_B_3/main.c
@@ -7,6 +7,24 @@
 
 #include <xc.h>
 
+void display(int digit ){
+	
+}
+
+const unsigned char Numbers [10] = { 
+	// dPgfe dcba 0b 0011 1111, 
+	// 0 0b 0000 0110, 
+	// 1 0b 0101 1011, 
+	// 2 0b 0100 1111, 
+	// 3 0b 0110 0110, 
+	// 4 0b 0110 1101, 
+	// 5 0b 0111 1101, 
+	// 6 0b 0000 0111, 
+	// 7 0b 0111 1111, 
+	// 8 0b 0110 1111, 
+	// 9 };
+
+
 int main(void)
 {
     while(1)
