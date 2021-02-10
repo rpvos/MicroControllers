68468b33f5077bba9abba97ed9272a60e4258993
Dave-Avans <dl.visser@student.avans.nl>
2021-02-10 11:26:02 +0100

    [Finish]: Finished opdracht 2.2 and added project 2.3

---
+++ Microcontroller_Project/Opdracht_Week2_B_3/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/10/2021 11:07:07 AM
+ *  Author: Dave-
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
