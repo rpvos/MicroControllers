c7d0747d241479eb16e8a42869a2b370f10b2873
Rik <rik.vos01@gmail.com>
2021-03-31 12:17:55 +0200

    [Add] eindproject to testcode

--- Microcontroller_Project/TestCode/main.c
+++ Microcontroller_Project/TestCode/main.c
@@ -1,12 +1,14 @@
 #include <avr/sfr_defs.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define F_CPU 8e6
 
 
 #define setbit(port,bit)  ((port) |=  (1<<bit)) // set bit
 #define clrbit(port,bit)  ((port) &= ~(1<<bit)) // clear bit
+#define BIT(x)			(1 << (x))
 
 #define REF_AVCC (1<<REFS0)  // reference = AVCC = 5 V
 #define REF_INT  (1<<REFS0)|(1<<REFS1) // internal reference 2.56 V
@@ -14,61 +16,83 @@
 // global variables
 uint16_t adc_value0, adc_value1, adc_value2, adc_value3, adc_value4, adc_value5;
 
+void wait( int ms ) {
+	for (int tms=0; tms<ms; tms++) {
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
+}
 
-//------------------------------------------------------
 uint16_t adc_read(uint8_t channel)
 {
 	ADMUX = REF_AVCC | channel;  // set reference and channel
 	ADCSRA |= (1<<ADSC);         // start conversion
 	loop_until_bit_is_clear(ADCSRA,ADSC); // wait for conversion complete
-	return ADC;
+	return ADC;		
 }
 
-//======================================================
+volatile int Frequency = 100;
+volatile int DutyCycle = 50;
+volatile int high = 0;
+
+ISR( TIMER1_COMPA_vect ) {
+	if (high)
+	{
+		int counter = Frequency*DutyCycle/100;
+		if (counter<1)
+		{
+			counter = 1;
+		}
+	OCR1A = counter;		// Set the compare value
+	
+	}else{
+		int counter =Frequency*(100-DutyCycle)/100;
+		if (counter<1)
+		{
+			counter = 1;
+		}
+	OCR1A = counter;		// Set the compare value
+		
+	}
+	TCNT0 = 0;
+	high ^= 0x01;
+	PORTB ^= 0x01;
+}
+
+void timer1Init( void ) {
+	OCR1A = Frequency;	// 16-bits compare value of counter 1
+	TIMSK |= BIT(4);		// T1 compare match A interrupt enable
+	sei();					// turn_on intr all	TCCR1A = 0b00000000;  Initialize T1: timer, prescaler=256,
+	TCCR1B = 0b00001100;	// compare output disconnected, CTC, RUN
+}
+
 
 int main(void)
 {
 	DDRB = 0xff; // portB all outputs
-	DDRF = 0x00;
+	DDRF = 0x00; // portF all inputs
 	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+	timer1Init();
+	sei();					// turn on all interrupts
 	
 	while(1)
 	{
 		adc_value0 = adc_read(0);
+		double percentage = adc_value0/1024.0;
+		double waitAmount = percentage*100;
+		Frequency = waitAmount;
+		
+		
 		adc_value1 = adc_read(1);
+		percentage = adc_value1/1024.0 *100;
+		DutyCycle = percentage;
+		
+		/*
 		adc_value2 = adc_read(2);
 		adc_value3 = adc_read(3);
 		adc_value4 = adc_read(4);
 		adc_value5 = adc_read(5);
 		
-		if(adc_value0 > 512)  // >2.5 V
-		setbit(PORTB,0);
-		else
-		clrbit(PORTB,0);
-		
-		if(adc_value1 > 512)
-		setbit(PORTB,1);
-		else
-		clrbit(PORTB,1);
-		
-		if(adc_value2 > 512)  // >2.5 V
-		setbit(PORTB,2);
-		else
-		clrbit(PORTB,2);
-		
-		if(adc_value3 > 512)
-		setbit(PORTB,3);
-		else
-		clrbit(PORTB,3);
-		
-		if(adc_value4 > 512)  // >2.5 V
-		setbit(PORTB,4);
-		else
-		clrbit(PORTB,4);
-		
-		if(adc_value5 > 512)
-		setbit(PORTB,5);
-		else
-		clrbit(PORTB,5);
+		CompareValue = adc_value0;
+		*/
 	}
 }
