7a951542f23dbff6bf22d808a628ff8800beb0fc
Rik <rik.vos01@gmail.com>
2021-03-31 09:25:46 +0200

    [Add] code to test project

--- Microcontroller_Project/TestCode/main.c
+++ Microcontroller_Project/TestCode/main.c
@@ -1,184 +1,74 @@
-#define F_CPU 8e6
-
+#include <avr/sfr_defs.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
 
-#define LCD_E 	6  // RA6 UNI-6
-#define LCD_RS	4  // RA4 UNI-6
-
-void lcd_strobe_lcd_e(void);
-void init_4bits_mode(void);
-void lcd_write_string(char *str);
-void lcd_write_data(unsigned char byte);
-void lcd_write_cmd(unsigned char byte);
-void lcd_clear(void);
+#define F_CPU 8e6
 
 
-/******************************************************************
-short:			Busy wait number of millisecs
-inputs:			int ms (Number of millisecs to busy wait)
-outputs:
-notes:			Busy wait, not very accurate. Make sure (external)
-clock value is set. This is used by _delay_ms inside
-util/delay.h
-Version :    	DMK, Initial code
-*******************************************************************/
-	void wait( int ms ) {
-		for (int i=0; i<ms; i++) {
-			_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
-		}
-	}
+#define setbit(port,bit)  ((port) |=  (1<<bit)) // set bit
+#define clrbit(port,bit)  ((port) &= ~(1<<bit)) // clear bit
 
-/******************************************************************
-short:			Strobe LCD module E pin --__
-inputs:
-outputs:
-notes:			According datasheet HD44780
-Version :    	DMK, Initial code
-*******************************************************************/
-void lcd_strobe_lcd_e(void) {
-	PORTA |= (1<<LCD_E);	// E high
-	_delay_ms(1);			// nodig
-	PORTA &= ~(1<<LCD_E);  	// E low
-	_delay_ms(1);			// nodig?
+#define REF_AVCC (1<<REFS0)  // reference = AVCC = 5 V
+#define REF_INT  (1<<REFS0)|(1<<REFS1) // internal reference 2.56 V
+
+// global variables
+uint16_t adc_value0, adc_value1, adc_value2, adc_value3, adc_value4, adc_value5;
+
+
+//------------------------------------------------------
+uint16_t adc_read(uint8_t channel)
+{
+	ADMUX = REF_AVCC | channel;  // set reference and channel
+	ADCSRA |= (1<<ADSC);         // start conversion
+	loop_until_bit_is_clear(ADCSRA,ADSC); // wait for conversion complete
+	return ADC;
 }
 
+//======================================================
 
-
-/******************************************************************
-short:			Init LCD module in 4 bits mode.
-inputs:
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-void init_4bits_mode(void) {
-	// PORTC output mode and all low (also E and RS pin)
-	DDRD = 0xFF;
-	DDRA = 0xFF;
-	PORTC = 0x00;
-	PORTA = 0x00;
-	//PORTA = 0xFF;
-
-	// Step 2 (table 12)
-	PORTC = 0x20;	// function set
-	lcd_strobe_lcd_e();
-
-	// Step 3 (table 12)
-	PORTC = 0x20;   // function set
-	lcd_strobe_lcd_e();
-	PORTC = 0x80;
-	lcd_strobe_lcd_e();
-
-	// Step 4 (table 12)
-	PORTC = 0x00;   // Display on/off control
-	lcd_strobe_lcd_e();
-	PORTC = 0xF0;
-	lcd_strobe_lcd_e();
-
-	// Step 4 (table 12)
-	PORTC = 0x00;   // Entry mode set
-	lcd_strobe_lcd_e();
-	PORTC = 0x60;
-	lcd_strobe_lcd_e();
-}
-
-/******************************************************************
-short:			Writes string to LCD at cursor position
-inputs:
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-void lcd_write_string(char *str) {
-	// Het kan met een while:
-
-	// while(*str) {
-	// 	lcd_write_data(*str++);
-	// }
-
-	// of met een for:
-	for(;*str; str++){
-		lcd_write_data(*str);
-	}
-}
-
-
-/******************************************************************
-short:			Writes 8 bits DATA to lcd
-inputs:			byte - written to LCD
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-void lcd_write_data(unsigned char byte) {
-	// First nibble.
-	PORTC = byte;
-	PORTA |= (1<<LCD_RS);
-	lcd_strobe_lcd_e();
-
-	// Second nibble
-	PORTC = (byte<<4);
-	PORTA |= (1<<LCD_RS);
-	lcd_strobe_lcd_e();
-}
-
-/******************************************************************
-short:			Writes 8 bits COMMAND to lcd
-inputs:			byte - written to LCD
-outputs:
-notes:			According datasheet HD44780 table 12
-Version :    	DMK, Initial code
-*******************************************************************/
-void lcd_write_command(unsigned char byte) {
-	// First nibble.
-	PORTC = byte;
-	PORTA &= ~(1<<LCD_RS);
-	lcd_strobe_lcd_e();
-
-	// Second nibble
-	PORTC = (byte<<4);
-	PORTA &= ~(1<<LCD_RS);
-	lcd_strobe_lcd_e();
-}
-
-
-void lcd_clear() {
-	lcd_write_command (0x01);						//Leeg display
-	_delay_ms(2);
-	lcd_write_command (0x80);						//Cursor terug naar start
-}
-
-
-/******************************************************************
-short:			main() loop, entry point of executable
-inputs:
-outputs:
-notes:			Slow background task after init ISR
-Version :    	DMK, Initial code
-*******************************************************************/
-int main( void ) {
-	// Init I/O
-	DDRC = 0xFF;			// PORTD(7) output, PORTD(6:0) input
-	PORTC = 0xFF;
-
-	// Init LCD
-	init_4bits_mode();
+int main(void)
+{
+	DDRB = 0xff; // portB all outputs
+	DDRF = 0x00;
+	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 	
-	_delay_ms(10);
-	
-	lcd_clear();
-
-	// Write sample string
-	char text[] = "maatje!";
-	lcd_write_string(text);
-
-	// Loop forever
-	while (1) {
-		PORTC ^= (1<<0);	// Toggle PORTD.7
-		wait( 250 );
+	while(1)
+	{
+		adc_value0 = adc_read(0);
+		adc_value1 = adc_read(1);
+		adc_value2 = adc_read(2);
+		adc_value3 = adc_read(3);
+		adc_value4 = adc_read(4);
+		adc_value5 = adc_read(5);
+		
+		if(adc_value0 > 512)  // >2.5 V
+		setbit(PORTB,0);
+		else
+		clrbit(PORTB,0);
+		
+		if(adc_value1 > 512)
+		setbit(PORTB,1);
+		else
+		clrbit(PORTB,1);
+		
+		if(adc_value2 > 512)  // >2.5 V
+		setbit(PORTB,2);
+		else
+		clrbit(PORTB,2);
+		
+		if(adc_value3 > 512)
+		setbit(PORTB,3);
+		else
+		clrbit(PORTB,3);
+		
+		if(adc_value4 > 512)  // >2.5 V
+		setbit(PORTB,4);
+		else
+		clrbit(PORTB,4);
+		
+		if(adc_value5 > 512)
+		setbit(PORTB,5);
+		else
+		clrbit(PORTB,5);
 	}
-
-	return 1;
 }
